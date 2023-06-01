0df50c1b3a7ea39b2978f46a189a60e96f79a7f9
Chetanjorwal3 <chetanjorwal3@gmail.com>
2023-06-01 21:56:51 +0530

    c

--- src/database/headerData.js
+++ src/database/headerData.js
@@ -1,11 +1,11 @@
 import T1 from "../assest/img/i1.png";
 import c3 from "../assest/img/c3.png";
-import f1 from "../assest/img/f1.png";
+import f1 from "../assest/img/images8.png";
 import r1 from "../assest/img/r1.png";
 
 export const headerData = [
   { id: "1673062197207", name: "Icecream", decs: "Chocolate & vanilla", price: "99", img: T1 },
-  { id: "1673062328128", name: "Strawberries", decs: "Fresh Strawberries", price: "199", img: f1 },
+  { id: "1673062328128", name: "Spaghetti", decs: "Spaghetti", price: "300", img: f1 },
   { id: "1673062261431", name: "Chicken kebab", decs: "Mixed Kebab Plate", price: "149", img: c3 },
   { id: "1673062408402", name: "Veg Pulav", decs: "Rice", price: "199", img: r1 },
 ];
