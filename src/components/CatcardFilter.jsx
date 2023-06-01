a145cb14deb2357b34eff6d49816b497994fe75f
Chetanjorwal3 <chetanjorwal3@gmail.com>
2023-06-01 21:31:45 +0530

    colors

--- src/components/CatcardFilter.jsx
+++ src/components/CatcardFilter.jsx
@@ -7,10 +7,10 @@ const CatCardFilter = ({ name, active = false, onClick }) => {
   return (
     <div>
       <div onClick={onClick} className={`group flex justify-center items-center p-2 ${active ? " bg-black" : "bg-black"} text-purple-300 transition-all flex-col font__5 rounded min-w-[75px] h-20 cursor-pointer hover:scale-105 mr-3 mt-1 drop-shadow `}>
-        <div className="text-6xl  bg-black p-2 text-red-600 drop-shadow hover:text-orange-200">
+        <div className={`text-6xl  bg-black p-2 drop-shadow hover:text-orange-200 ${active ? " text-orange-500" : ""}`}>
           <BsShopWindow />
         </div>
-        <h5 className={`mt-0 ${active ? " text-purple-300" : ""} group-hover:text-cyan-300 capitalize`}>{name}</h5>
+        <h5 className={`${active ? " text-orange-200" : ""} group-hover:text-cyan-300 capitalize`}>{name}</h5>
       </div>
     </div>
   );
